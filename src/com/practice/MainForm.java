380121d403f835cdbdc9142eae59af81dbd5b950
Sanskrutikondke <sanskrutikondke@gmail.com>
2024-12-27 13:54:07 +0530

    First commit

---
+++ src/com/practice/MainForm.java
@@ -0,0 +1,9 @@
+package com.practice;
+
+
+public class MainForm {
+    public static void main(String[] args) {
+        System.out.println("Testing....");
+        Form f = new Form();
+    }
+}
---
+++ src/com/practice/MainMyWindow.java
@@ -0,0 +1,8 @@
+package com.practice;
+
+public class MainMyWindow {
+    public static void main(String[] args) {
+        System.out.println("testing...");
+        MyWindow w = new MyWindow();
+    }
+}
