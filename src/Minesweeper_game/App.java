d4d8c106fc1957e4f0cd62e17b5f9db53682ac88
Sanskrutikondke <sanskrutikondke@gmail.com>
2025-01-22 15:06:10 +0530

    First commit

---
+++ src/Minesweeper_game/App.java
@@ -0,0 +1,7 @@
+package Minesweeper_game;
+
+public class App {
+    public static void main(String[] args)throws Exception {
+        Minesweeper minesweeper = new Minesweeper();
+    }
+}
---
+++ src/Tic_Tac_Toe_game/App.java
@@ -0,0 +1,7 @@
+package Tic_Tac_Toe_game;
+
+public class App {
+    public static void main(String[] args) {
+        TicTacToe ticTacToe = new TicTacToe();
+    }
+}
